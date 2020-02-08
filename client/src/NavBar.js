701b0d14d0ac78279738b4c07b089c6f7fb75339
Trevor Stenson <stenson124@gmail.com>
2020-02-08 05:31:41 -0500

    client with login page

---
+++ client/src/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">comread</Navbar.Brand>
+            <Navbar.Collapse id="basic-navbar-nav" className="float-right">
+                <Nav className="ml-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
