45798cf385b47602c56fbe34009b4b2b5eef9c94
Shawn Dermer <shawn.dermer@gmail.com>
2020-02-08 12:11:31 -0500

    Essay page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar.js'
 import Home from './components/Home.js'
 import Login from './components/Login.js'
 import Account from './components/Account.js'
+import Essay from './components/Essay.js'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css'
@@ -17,6 +18,7 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/account' component={Account} />
+        <Route exact path='/essay' component={Essay} />
         </Switch>
       </div>
   </Router>
