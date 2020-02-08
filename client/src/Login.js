0550cd65195ff2f7b5ac26be68fb76da74fae35d
Trevor Stenson <stenson124@gmail.com>
2020-02-08 06:14:46 -0500

    finished login workflow

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,45 +1,76 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, FormLabel, Card } from "react-bootstrap";
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import AuthService from './auth/authService'
 import "./Login.css";
 
-export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default class Login extends Component {
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    }
+    this.login = this.login.bind(this);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  componentDidMount() {
+    localStorage.clear();
   }
 
-  return (
-    <div className="Login">
+  login = (e) => {
+    //e.preventDefault();
+    const credentials = {email: this.state.email, password: this.state.password};
+    AuthService.login(credentials).then(res => {
+      console.log('then!!')
+      console.log(res.data)
+        if(res.status === 200){
+            localStorage.setItem("userInfo", JSON.stringify(res.data));
+            console.log(localStorage.getItem("userInfo"))
+            //this.props.history.push('/account');
+        }else {
+            this.setState({message: res.data.message});
+        }
+    });
+};
 
-        <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
- 
-    </div>
-  );
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  render() {
+    return (
+      <div className="Login">
+          <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.onChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              name="password"
+              value={this.state.password}
+              onChange={this.onChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button block bsSize="large" disabled={!this.validateForm()} onClick={this.login}>
+            Login
+          </Button>
+        </form>
+   
+      </div>
+    );
+  }
+  
 }
