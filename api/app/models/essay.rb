de287ec4306204926d65fe33097f61763b87a3bd
Shawn Dermer <shawn.dermer@gmail.com>
2020-02-09 02:17:53 -0500

    only give chunks to ready

--- api/app/models/essay.rb
+++ api/app/models/essay.rb
@@ -10,14 +10,14 @@ class Essay < ApplicationRecord
       chunk_bodies = body.split(/(?<=[?.!])/)
       i = 0
       chunk_bodies.each do |chunk_body|
-        r_id = User.offset(rand(User.count)).first.id
-        while r_id == user_id
-            r_id = User.offset(rand(User.count)).first.id
+        r = User.offset(rand(User.count)).first
+        while r.id == user_id || r.ready
+            r = User.offset(rand(User.count)).first
         end    
         Chunk.create(
             user_id: user_id,
             body: chunk_body,
-            reviewer_id: r_id,
+            reviewer_id: r.id,
             index: i,
             rating: false
         )
