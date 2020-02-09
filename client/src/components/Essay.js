dd803e574ffb06ece07a4a31bfdd81687f2aecf3
Trevor Stenson <stenson124@gmail.com>
2020-02-09 01:58:45 -0500

    essay workflow done

--- client/src/components/Essay.js
+++ client/src/components/Essay.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import EssayService from '../essays/essayService'
+import UserService from '../users/userService'
 import { Card, Button } from 'react-bootstrap'
 import './Essay.css'
 
@@ -7,23 +8,40 @@ export default class Essay extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            essay: null,
-            essaySubmitted: false
+            essay: '',
+            submissionFailed: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    submitEssay = () => {
+        if (JSON.parse(localStorage.getItem("userInfo")).score >= 50) {
+            EssayService.createEssay(JSON.parse(localStorage.getItem("userInfo")), this.state.essay).then(res => {
+                console.log('DONE CREATING ESSAY')
+                if (res.status == 201) {
+                    UserService.spendCreditsOnEssay(JSON.parse(localStorage.getItem("userInfo"))).then(res => {
+                        this.props.history.push('/account')
+                    })
+                }
+                //this.setState({ essay: res.data, essaySubmitted: true });
+            }).catch(error => {
+                console.log(error)
+                this.setState({submissionFailed: true})
+            })
         }
     }
 
-    submitEssay() {
-        EssayService.createEssay(JSON.parse(localStorage.getItem("userInfo")), this.state.essay).then(res => {
-            this.setState({ essay: res.data, essaySubmitted: true });
-        });
+    handleChange(e) {
+        this.setState({ essay: e.target.value })
     }
 
     render() {
         return (
             <Card className="essay-center">
                 <h3 id="header">Paste your essay below, and click Submit to submit.</h3>
-                <textarea value={this.state.essay} type="text" className="essaybox"></textarea>
-                <Button variant="secondary" size="lg" onClick={this.submitEssay()}>Submit</Button>
+                <textarea value={this.state.essay} onChange={this.handleChange} type="text" className="essaybox"></textarea>
+                {this.state.submissionFailed ? <div id="failMsg">Submission failed. You have already submitted an essay. Please wait for feedback.</div> : null}
+                <Button variant="secondary" disabled={this.state.submissionFailed} size="lg" onClick={this.submitEssay}>Submit</Button>
             </Card>
         )
     }
--- client/src/essays/essayService.js
+++ client/src/essays/essayService.js
@@ -3,10 +3,9 @@ import axios from 'axios'
 const ESSAY_API_BASE_URL = 'http://localhost:5000/essay/';
 
 class EssayService {
-    createEssay(essayInfo) {
-        return axios.post(ESSAY_API_BASE_URL, essayInfo)
+    createEssay(userInfo, essayInfo) {
+        return axios.post(ESSAY_API_BASE_URL, {user_id: userInfo.user_id, body: essayInfo}, { headers: { "Authorization" : userInfo.token } })
     }
-    
 }
 
 export default new EssayService();
