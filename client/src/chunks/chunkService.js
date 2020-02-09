161dc4eb2d18f70e7acd3508905a602a011b34e1
Shawn Dermer <shawn.dermer@gmail.com>
2020-02-09 09:49:08 -0500

    mmmvp

--- client/src/chunks/chunkService.js
+++ client/src/chunks/chunkService.js
@@ -12,6 +12,10 @@ class ChunkService {
         return axios.get(CHUNK_API_BASE_URL + 'allchunks/' + userInfo.user_id, 
         { headers: { "Authorization" : userInfo.token } })
     }
+
+    putFeedbackOnChunk(userInfo, chunk_id, fb) {
+        return axios.post(CHUNK_API_BASE_URL + 'chunk/' + chunk_id + '-' + fb)
+    }
 }
 
 export default new ChunkService();
--- client/src/components/FeedbackList.js
+++ client/src/components/FeedbackList.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react'
+import { Form } from 'react-bootstrap'
 import './FeedbackList.css'
+import ChunkService from '../chunks/chunkService'
 var moment = require('moment');
 
 export default class FeedbackList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            textInput: '',
+            chunkId: 0,
+            chunkUserId: 0
+        };
+        this.renderFeedbackItem = this.renderFeedbackItem.bind(this)
+        this.myHandleSubmit = this.myHandleSubmit.bind(this);
+    }
+    
+    myHandleSubmit(e) {
+        e.preventDefault();
+        console.log(this.textInput);
+        console.log(this.chunkId);
+        console.log(JSON.parse(localStorage.getItem("userInfo")));
+        ChunkService.putFeedbackOnChunk(JSON.parse(localStorage.getItem("userInfo")), this.chunkId, this.textInput);
+    }
 
     renderFeedbackItem(chunk) {
         var timestamp = moment(chunk.created_at, 'YYYY-MM-DD');
-        chunk.created_at = timestamp.fromNow()
+        var created_at = timestamp.fromNow()
         return (
             <div key={chunk.id} user-id={chunk.user_id} className="row">
-                <div className="date">{chunk.created_at}</div>
+                <div className="date">
+                    {created_at}<br></br>
+                    {chunk.body}
+                </div>
+                <div>
+                <Form onSubmit={this.myHandleSubmit}>
+                    <Form.Group controlId="response for {chunk.id}">
+                        <Form.Control type="textarea" placeholder="Enter Feedback" onChange = {(t) => 
+                            {this.textInput = t.currentTarget.value;
+                             this.chunkId = chunk.id;
+                             this.chunkUserId = chunk.user_id;
+                            }}/>
+                    </Form.Group>
+                </Form>
+                </div>
                 <p id="score">+5</p>
             </div>
         )
